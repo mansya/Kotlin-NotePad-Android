31eb01d2f2be64f39b25a72dd1c3b474cd3f7764
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-08-04 13:04:38 +0800

    seek bar

--- app/src/main/java/com/freeankit/freenotepad/view/CreateActivity.kt
+++ app/src/main/java/com/freeankit/freenotepad/view/CreateActivity.kt
@@ -11,7 +11,6 @@ import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.RelativeLayout
 import android.widget.SeekBar
 import com.freeankit.freenotepad.R
 import com.freeankit.freenotepad.helper.showSnackbar
